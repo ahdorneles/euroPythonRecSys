ee2df9386b590e81df528c8ab85c65211290cd77
ahdorneles <ahdorneles@gmail.com>
2019-07-10 07:12:47 +0200

    First commit

---
+++ reccomendation.py
@@ -0,0 +1,36 @@
+score = {'Adriana':
+             {'The Devil wears Prada': 3.0,
+              'The Wolf of Wall Street': 4.0,
+              'The Lord of the Rings': 2.0},
+         
+         'Dani':
+             {'The Devil wears Prada': 4.0,
+              'The Lord of the Rings': 3.0,
+              'Baby Driver': 4.0},         
+          
+          'Henry':
+             {'The Devil wears Prada': 5.0,
+              'The Wolf of Wall Street': 5.0,
+              'Cool Runnings': 4.0,
+              'The Lord of the Rings': 4.0},
+           
+         'Marta':
+             {'The Devil wears Prada': 3.0,
+              'The Wolf of Wall Street': 3.0,
+              'Cool Runnings': 4.0,
+              'The Lord of the Rings': 3.0,
+              'Baby Driver': 4.0},
+         
+          'Philip':
+             {'The Devil wears Prada': 3.0,
+              'The Wolf of Wall Street': 4.5,
+              'Cool Runnings': 4.0,
+              'The Lord of the Rings': 4.5,
+              'Baby Driver': 4.0},
+           
+          'Ota':
+             {'The Devil wears Prada': 2.5,
+              'The Wolf of Wall Street': 3.5,
+              'Cool Runnings': 3.0,
+              'Baby Driver': 4.5}
+        }
